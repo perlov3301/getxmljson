6d3b5d64d0e8b53c865ed73f29fb3f611e97bf5b
Shlomo-Alexander Perlov <perlov3301@gmail.com>
2020-04-29 14:41:05 +0300

    Update dom.php

--- dom.php
+++ dom.php
@@ -1,7 +1,7 @@
 <?php
-  header ("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-  header ("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-  header ("Allow: GET, POST, OPTIONS, PUT, DELETE");
+//   header ("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+//   header ("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+//   header ("Allow: GET, POST, OPTIONS, PUT, DELETE");
   foreach($_POST as $post_var) {
       echo strtoupper($post_var) . "<br />";
   }
